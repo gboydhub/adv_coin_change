6f38bfeb41ee24f6222abe7aca620808088440dc
Gary Boyd <eponick@gmail.com>
2018-07-13 23:27:54 -0400

    Added coin conversion/redirects

--- app.rb
+++ app.rb
@@ -4,13 +4,27 @@ require_relative 'coin_exchange.rb'
 enable :sessions
 
 get '/' do
-  
+  session[:cents] = -1
   erb :default
 end
 
-post '/' do
-  session['cents'] = params['cents']
-  redirect '/'
+post '/change' do
+  session[:cents] = params['cents']
+  redirect '/change'
+end
+
+get '/change' do
+  if session[:cents].to_i <= 0
+    redirect '/'
+  end
+  converted_coins = ''
+  coin_exchange(session[:cents].to_i).each_pair do |key, val|
+    converted_coins += val.to_s
+    converted_coins += ' '
+    converted_coins += key.to_s.capitalize
+    converted_coins += '<br>'
+  end
+  erb :default, :locals => {:coins => converted_coins}
 end
 # we want to continue with the coin changer app.
 # When we enter the amount of change to process we want to see a pop up box
