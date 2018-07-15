e09273219b25bcdcda32ed6a7e3210afe049b7eb
Eponick <mikehuntwife@gmail.com>
2018-07-15 18:39:34 -0400

    Finished project

--- app.rb
+++ app.rb
@@ -26,10 +26,3 @@ get '/change' do
   end
   erb :default, :locals => {:coins => converted_coins}
 end
-# we want to continue with the coin changer app.
-# When we enter the amount of change to process we want to see a pop up box
-# that returns current date and time.
-
-# Then on the results page we want to see the returned coins along with the date and time.
-# Next we want several new background images on the results page received dynamically from js.
-# Next when hovering over results div make a stye change occur via js.
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,55 @@
-function popupDate()
+function getDateString()
 {
     var d = new Date();
     var month = ["January", "February", "March", "April",
         "May", "June", "July", "August", "September",
         "October", "November", "December"];
-    var curMonth = d.getMonth() + 1;
+    var curMonth = month[d.getMonth()];
     var curDay = d.getDate();
+    var curDaySuffix = "th";
+    switch(curDay%10)
+    {
+        case 1:
+        {
+            curDaySuffix = "st";
+            break;
+        }
+        case 2:
+        {
+            curDaySuffix = "nd";
+            break;
+        }
+        case 3:
+        {
+            curDaySuffix = "rd";
+            break;
+        }
+    }
     var curHour = (d.getHours() % 12);
-    var curMin = d.getMinutes();
-    var dateString = curMonth.toString() + '/' + curDay.toString() + '/' + d.getFullYear() +
-            ' ' + curHour.toString() + ':' + curMin.toString();
+    var curHourSuffix = "AM";
+    if(d.getHours() >= 12) curHourSuffix = "PM";
+    if(curHour == 0) curHour = 12;
 
-    window.alert(dateString);
+    var curMin = d.getMinutes();
+    var curMinPrefix = "";
+    if(curMin < 10) curMinPrefix = "0";
+
+    var dateString = curMonth.toString() + ' ' + curDay.toString() + curDaySuffix + ', ' + d.getFullYear() +
+            ' ' + curHour.toString() + ':' + curMinPrefix + curMin.toString() + ' ' + curHourSuffix;
+
+    return dateString;
+}
+
+function changeResults(newStyle)
+{
+    document.getElementById("coin_result").style = newStyle;
+}
+
+function setRandomBG()
+{
+    var randomBG = Math.floor((Math.random() * 2) + 1);
+    var bgURL = "url('/images/bg" + randomBG.toString() + ".gif')";
+    document.body.style.backgroundImage = bgURL;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
 }
